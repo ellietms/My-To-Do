4ebe3573bc1cc41a7a19f9a1a4c9025908a91513
eli <ellie.tms1371@gmail.com>
2021-09-13 14:51:26 +0100

    added database

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,49 @@
 from flask import Flask
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
+# Add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///toDos.db'
+# Secret Key
+app.config['SECRET_KEY'] = 'my to-do secret key'
+# initialize database
+todoDatbase = SQLAlchemy(app)
 
-engine = sqlalchemy.create_engine('mysql://root:EllieSql92@server')
-engine.execute("CREATE DATABASE todoList")
-engine.execute("USE todoList")
+# create Model
+class ToDos(todoDatbase.Model):
+    id = todoDatbase.Column(todoDatbase.Integer, primary_key = True)
+    name = todoDatbase.Column(todoDatbase.String(200), nullable = False, unique = True) 
+    date_added = todoDatbase.Column(todoDatbase.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<ToDos %r>' % self.name
 
 
-@app.route("/")
-def home():
+# Form with flask
+# class UserToDo(FlaskForm):
+#     name = StringField('name', validators=[DataRequired()])
+#     submit = SubmitField('Submit')
+
+
+@app.route("/todo/add", methods = ['GET', 'POST'])
+def add_todo():
     return "WORKING"
 
 
+
+    
+def run():
+    todoDatbase.create_all()    
+    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
