6848fc2ab472ed8641e935ed87563a0af86a898c
eli <ellie.tms1371@gmail.com>
2021-11-04 02:31:54 +0000

    update: change usestate name

--- Python-backend/routes.py
+++ Python-backend/routes.py
@@ -5,66 +5,64 @@ from app import app, todoDatbase
 from models import ToDos
 from datetime import datetime
 
+
 @app.route("/my-to-do-lists/", methods=['POST', 'GET'])
 def home():
     print("********************************")
     all_data_from_class_model_ToDos = ToDos.query.all()
     for eachData in all_data_from_class_model_ToDos:
-        print("ALL DATA" , eachData.to_json())
+        print("ALL DATA", eachData.to_json())
     print("*******************************")
     if request.method == 'POST':
         new_data = request.form.to_dict(flat=False)['toDoList'][0]
         print("New Data POST", new_data)
-        new_class_model_for_newData_postReq = ToDos(name = new_data)
+        new_class_model_for_newData_postReq = ToDos(name=new_data)
         todoDatbase.session.add(new_class_model_for_newData_postReq)
         todoDatbase.session.commit()
     elif request.method == 'GET':
         all_data = todoDatbase.session.query(ToDos).all()
         return jsonify({"todos": [each_data.to_json() for each_data in all_data]})
-        
+
     else:
         return "SOMETHING WENT WRONG"
-    
-    
-@app.route('/my-to-do-lists/<int:index>', methods= ['GET'])
+
+
+@app.route('/my-to-do-lists/<int:index>', methods=['GET'])
 def show_chosen_data(index):
     print("////////////////////////////////")
-     # first way filter
-    chosen_data_1= ToDos.query.get(index)
+    # first way filter
+    chosen_data_1 = ToDos.query.get(index)
     #  second way filter
     chosen_data_2 = ToDos.query.filter(ToDos.id == index).first()
     #  Third way filter
-    chosen_data_3 = todoDatbase.session.query(ToDos).filter(ToDos.id == index).first()
+    chosen_data_3 = todoDatbase.session.query(
+        ToDos).filter(ToDos.id == index).first()
     # 4th way filter
-    chosen_data_4 = ToDos.query.filter_by(id = index).first()
-    
-    print("-------RESULT1---------",chosen_data_1.to_json())
-    print("-------RESULT2---------",chosen_data_2.to_json())
-    print("-------RESULT3---------",chosen_data_3.to_json())
-    print("-------RESULT4---------",chosen_data_4.to_json())
-    
-    return  jsonify({"todos": chosen_data_1.to_json()})
+    chosen_data_4 = ToDos.query.filter_by(id=index).first()
+
+    print("-------RESULT1---------", chosen_data_1.to_json())
+    print("-------RESULT2---------", chosen_data_2.to_json())
+    print("-------RESULT3---------", chosen_data_3.to_json())
+    print("-------RESULT4---------", chosen_data_4.to_json())
+
+    return jsonify({"todos": chosen_data_1.to_json()})
 
     # Manual filtering
     # all_data_json = [each_data.to_json() for each_data in all_data]
     # chosen_data = [chosen_data for chosen_data in all_data_json if chosen_data['id'] != index]
-   
 
 
-
-@app.route("/my-to-do-lists/<int:index>", methods = ['DELETE'])    
+@app.route("/my-to-do-lists/<int:index>", methods=['DELETE'])
 def remove_chosen_data(index):
     chosen_data_to_delete = ToDos.query.get(index)
-    print("Data for deleting",chosen_data_to_delete.to_json())
+    print("Data for deleting", chosen_data_to_delete.to_json())
     todoDatbase.session.delete(chosen_data_to_delete)
     todoDatbase.session.commit()
-    all_updated_data = ToDos.query.all()            
+    all_updated_data = ToDos.query.all()
     for eachData in all_updated_data:
-            print("ALL DATA" , eachData.to_json())
-    return jsonify({"todos": chosen_data_to_delete.to_json()}) 
-    
-    
-
+        print("ALL DATA")
+        print("---", eachData.to_json(), "---")
+    return jsonify({"todos": chosen_data_to_delete.to_json()})
 
 
 if __name__ == "__main__":
--- React-to-do-list/src/App.js
+++ React-to-do-list/src/App.js
@@ -5,7 +5,7 @@ import ToDoLists from "./components/ToDoLists";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [newToDo, setNewToDo] = useState([]);
+  const [toDo, setToDo] = useState([]);
 
   useEffect(() => {
     fetch("/my-to-do-lists/", {
@@ -15,44 +15,32 @@ function App() {
       },
     })
       .then((res) => res.json())
-      .then((result) =>
-        setNewToDo(
-          [...newToDo,
-          ...result["todos"]]
-        )
-      )
-      .then((show) => console.log("newTodo",show));
-  },[]);
-
+      .then((result) => setToDo([...toDo, ...result["todos"]]))
+      .then((show) => console.log("Todo", show));
+  }, []);
 
   const handleInputValue = (event) => {
     setInputValue(event.target.value);
   };
 
-  const handleDeleteItem = (index) => { 
-    console.log("index",index)   
-    fetch(`/my-to-do-lists/${index}` , {
-      method: 'DELETE',
-      headers : {      
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
+  const handleDeleteItem = (index) => {
+    console.log("index", index);
+    fetch(`/my-to-do-lists/${index}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
     })
-    .then((res) => res.json())  
-    .then((result) => setNewToDo(result["todos"]))
+      .then((res) => res.json())
+      .then((result) => setToDo(result["todos"]));
   };
 
   return (
     <div className="App">
       <h1 className="main_label"> My To-Do</h1>
-      <Form
-        inputValue={inputValue}
-        handleInputValue={handleInputValue}
-      />
-      <ToDoLists 
-      list={newToDo}
-      handleDeleteItem={handleDeleteItem}
-      />
+      <Form inputValue={inputValue} handleInputValue={handleInputValue} />
+      <ToDoLists list={toDo} handleDeleteItem={handleDeleteItem} />
     </div>
   );
 }
